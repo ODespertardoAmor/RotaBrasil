9c8ff109604f0736ed8ded1b18fe397ca156c072
ODespertardoAmor <escritorairanelmalemos@gmail.com>
2026-05-28 18:27:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -504,8 +504,8 @@ def minhas_avaliacoes():
 
     return jsonify(avaliacoes)
 if __name__ == "__main__":
- #  with app.app_context():
-        #db.create_all()
+   with app.app_context():
+        db.create_all()
     
     port = int(os.environ.get("PORT", 5000))
     # 🔧 MODO DEBUG DESLIGADO PARA PRODUÇÃO NO RENDER (MAIS ESTÁVEL)
